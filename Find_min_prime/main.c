082ede3aaea6ac4f091ecfd20b3a7b7315b31b96
kristof-levai <kristof.levai@gmail.com>
2023-08-03 10:34:51 +0200

    Add files via upload

---
+++ Find_min_prime/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    printf("Give me 10 positive whole nums: \n");
+    for (size_t i = 0; i < 10; i++)
+    {
+        scanf("%i", &arr[i]);
+    }
+    
+        
+    return 0;
+}
